c48f71af49f48fa2a3508d42f7abbea432b9ec48
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2020-06-02 00:44:26 +0630

    Add Buyer Product Collection

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,8 @@ use Illuminate\Support\Facades\Route;
 |----------
 */
 Route::resource('buyers', 'Buyer\BuyerController')->only(['index', 'show']);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController')->only(['index']);
+Route::resource('buyers.products', 'Buyer\BuyerProductController')->only(['index']);
 
 /*
 |----------
